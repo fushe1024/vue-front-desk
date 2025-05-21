047f84a8ea5109966780fec3959c51bd64153909
Cookie <2384485535@qq.com>
2025-05-21 15:55:15 +0800

    chore: update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -23,17 +23,16 @@ export default defineConfig({
     'subject-case': [0, 'never', []]
   },
   prompt: {
-    alias: { fd: 'docs: fix typos' },
     messages: {
       type: '选择你要提交的类型 :',
       scope: '选择一个提交范围（可选）:',
       customScope: '请输入自定义的提交范围 :',
-      subject: '填写简短精炼的变更描述 :\n',
-      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      subject: '填写简短精炼的变更描述 :',
+      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :',
+      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :',
       footerPrefixesSelect: '选择关联issue前缀（可选）:',
       customFooterPrefix: '输入自定义issue前缀 :',
-      footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
+      footer: '列举关联issue (可选) 例如: #31, #I3244 :',
       confirmCommit: '是否提交或修改commit ?'
     },
     types: [
@@ -74,37 +73,6 @@ export default defineConfig({
       }
     ],
     useEmoji: false,
-    emojiAlign: 'center',
-    useAI: false,
-    aiNumber: 1,
-    themeColorCode: '',
-    scopes: [],
-    allowCustomScopes: true,
-    allowEmptyScopes: true,
-    customScopesAlign: 'bottom',
-    customScopesAlias: 'custom',
-    emptyScopesAlias: 'empty',
-    upperCaseSubject: false,
-    markBreakingChangeMode: false,
-    allowBreakingChanges: ['feat', 'fix'],
-    breaklineNumber: 100,
-    breaklineChar: '|',
-    skipQuestions: [],
-    issuePrefixes: [
-      // 如果使用 gitee 作为开发管理
-      { value: 'link', name: 'link:     链接 ISSUES 进行中' },
-      { value: 'closed', name: 'closed:   标记 ISSUES 已完成' }
-    ],
-    customIssuePrefixAlign: 'top',
-    emptyIssuePrefixAlias: 'skip',
-    customIssuePrefixAlias: 'custom',
-    allowCustomIssuePrefix: true,
-    allowEmptyIssuePrefix: true,
-    confirmColorize: true,
-    scopeOverrides: undefined,
-    defaultBody: '',
-    defaultIssues: '',
-    defaultScope: '',
-    defaultSubject: ''
+    emojiAlign: 'left'
   }
 })
